77e04e87c53f184056c33182cc67bcfc0b07a475
storycraft <storycraft@pancake.sh>
2020-10-04 15:48:46 +0900

    Update example with simple command

--- src/main/java/sh/pancake/plugin/example/ExamplePlugin.java
+++ src/main/java/sh/pancake/plugin/example/ExamplePlugin.java
@@ -1,5 +1,13 @@
 package sh.pancake.plugin.example;
 
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.FloatArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.Explosion.BlockInteraction;
 import sh.pancake.server.plugin.IPancakePlugin;
 import sh.pancake.server.plugin.PluginData;
 
@@ -15,13 +23,29 @@ public class ExamplePlugin implements IPancakePlugin {
     }
 
     @Override
-    public void onLoad() {
-        
-    }
+    public void onServerPreInit() {
+        CommandDispatcher<CommandSourceStack> dispatcher = data.getServer().getCommandManager().getDispatcherFor(this);
 
-    @Override
-    public void onUnload() {
-        
+        // Registering sample explode commands
+        dispatcher.register(
+            LiteralArgumentBuilder.<CommandSourceStack>literal("explode")
+            // Check if executor has op
+            .requires((source) -> source.hasPermission(3))
+            // Limit 0.0 to max 100.0
+            .then(RequiredArgumentBuilder.<CommandSourceStack, Float>argument("power", FloatArgumentType.floatArg(0.0f, 100.0f))
+                .executes((ctx) -> {
+                    float power = ctx.getArgument("power", Float.class);
+
+                    CommandSourceStack source = ctx.getSource();
+                    Entity entity = source.getEntity();
+                    
+                    source.getLevel().explode(entity, entity.getX(), entity.getY(), entity.getX(), power, BlockInteraction.DESTROY);
+                    
+                    // We executed once
+                    return 1;
+                })
+            )
+        );
     }
 
 }
