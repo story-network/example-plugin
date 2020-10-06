9ff966591287648f5ff8369ba4e7b0eef13abcbc
storycraft <storycraft@pancake.sh>
2020-10-07 01:15:35 +0900

    update command api

--- src/main/java/sh/pancake/plugin/example/ExamplePlugin.java
+++ src/main/java/sh/pancake/plugin/example/ExamplePlugin.java
@@ -5,12 +5,13 @@ import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 
-import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Explosion.BlockInteraction;
+import net.minecraft.commands.CommandSourceStack;
 import sh.pancake.server.plugin.IPancakePlugin;
 import sh.pancake.server.plugin.PluginData;
+import sh.pancake.server.command.ICommandStack;
 
 public class ExamplePlugin implements IPancakePlugin {
 
@@ -25,19 +26,19 @@ public class ExamplePlugin implements IPancakePlugin {
 
     @Override
     public void onServerPreInit() {
-        CommandDispatcher<CommandSourceStack> dispatcher = data.getServer().getCommandManager().getDispatcherFor(this);
+        CommandDispatcher<ICommandStack> dispatcher = data.getServer().getCommandManager().getDispatcherFor(this);
 
         // Registering sample explode commands
         dispatcher.register(
-            LiteralArgumentBuilder.<CommandSourceStack>literal("explode")
+            LiteralArgumentBuilder.<ICommandStack>literal("explode")
             // Check if executor has op
             .requires((source) -> source.hasPermission(3))
             // Limit 0.0 to max 100.0
-            .then(RequiredArgumentBuilder.<CommandSourceStack, Float>argument("power", FloatArgumentType.floatArg(0.0f, 100.0f))
+            .then(RequiredArgumentBuilder.<ICommandStack, Float>argument("power", FloatArgumentType.floatArg(0.0f, 100.0f))
                 .executes((ctx) -> {
                     float power = ctx.getArgument("power", Float.class);
 
-                    CommandSourceStack source = ctx.getSource();
+                    CommandSourceStack source = ctx.getSource().getSourceStack();
                     Entity entity = source.getEntity();
 
                     if (entity == null) {
